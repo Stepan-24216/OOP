4db6048f1f68dcd7b02d674debfecdee4ab729a0
StepanEfimov <s.efimov@g.nsu.ru>
2025-09-20 19:26:26 +0700

    add test and code style

--- Task_1_3/src/main/java/org/example/Mul.java
+++ Task_1_3/src/main/java/org/example/Mul.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Mul extends Expression{
+public class Mul extends Expression {
     Expression element1;
     Expression element2;
 
@@ -8,17 +8,21 @@ public class Mul extends Expression{
         this.element1 = left;
         this.element2 = right;
     }
-    public String toString(){
-        return "(" + element1+ " * " + element2 + ")";
+
+    public String toString() {
+        return "(" + element1 + " * " + element2 + ")";
     }
 
-    public Expression derivative(String variable){
-        if (!(element1 instanceof Number) && element2 instanceof Number){return new Mul(element1.derivative(variable),element2);}
-        else if (element1 instanceof Number && !(element2 instanceof Number)){return new Mul(element1,element2.derivative(variable));}
-        return new Add( new Mul(element1.derivative(variable),element2), new Mul(element1,element2.derivative(variable)));
+    public Expression derivative(String variable) {
+        if (!(element1 instanceof Number) && element2 instanceof Number) {
+            return new Mul(element1.derivative(variable), element2);
+        } else if (element1 instanceof Number && !(element2 instanceof Number)) {
+            return new Mul(element1, element2.derivative(variable));
+        }
+        return new Add(new Mul(element1.derivative(variable), element2), new Mul(element1, element2.derivative(variable)));
     }
 
-    public int eval(String s){
+    public int eval(String s) {
         return element1.eval(s) * element2.eval(s);
     }
 }
