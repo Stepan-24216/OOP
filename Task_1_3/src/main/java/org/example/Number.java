a54084a5d1dd706fcbc0520e861642ef0bdb4d64
StepanEfimov <s.efimov@g.nsu.ru>
2025-09-28 19:15:22 +0700

    code style

--- Task_1_3/src/main/java/org/example/Number.java
+++ Task_1_3/src/main/java/org/example/Number.java
@@ -4,29 +4,29 @@ package org.example;
  * Реализация методов для чисел.
  */
 public class Number extends Expression {
-  private final int value;
+    private final int value;
 
-  public Number(int value) {
-    this.value = value;
-  }
-
-  public int getValue() {
-    return value;
-  }
-
-  public Expression derivative(String variable) {
-    return new Number(0); // производная константы это нуль
-  }
-
-  public String toString() {
-    if (value >= 0) {
-      return String.valueOf(value);
-    } else {
-      return ("(" + value + ")");
+    public Number(int value) {
+        this.value = value;
     }
-  }
 
-  public int eval(String s) {
-    return value;
-  }
+    public int getValue() {
+        return value;
+    }
+
+    public Expression derivative(String variable) {
+        return new Number(0); // производная константы это нуль
+    }
+
+    public String toString() {
+        if (value >= 0) {
+            return String.valueOf(value);
+        } else {
+            return ("(" + value + ")");
+        }
+    }
+
+    public int eval(String s) {
+        return value;
+    }
 }
