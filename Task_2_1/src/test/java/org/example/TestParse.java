f288d4de56d2192ce5d23d6575548fe990750dfd
StepanEfimov <s.efimov@g.nsu.ru>
2025-10-11 21:45:31 +0700

    tests

--- Task_2_1/src/test/java/org/example/TestParse.java
+++ Task_2_1/src/test/java/org/example/TestParse.java
@@ -45,4 +45,64 @@ public class TestParse {
             i++;
         }
     }
+
+    @Test
+    void testParseNoEdge() {
+        Graph graph2 = new AdjacencyMatrix();
+        String[] data = {
+            "E F",
+            "B F",
+            "B C",
+            "A B",
+            "C E",
+            "C D",
+            "A E"
+        };
+        String[] cur2 = {"E", "F", "B", "C", "A", "D"};
+        for (String line : data) {
+            ParseDataFile.parseData(line,graph2,false);
+        }
+        int i = 0;
+        for (Vertex vertex : graph2.getVertexList()) {
+            assertEquals(cur2[i], vertex.getName());
+            i++;
+        }
+        i = 0;
+        graph2.topologicalSort();
+        String[] sort2 = {"A", "B", "C", "D", "E", "F"};
+        for (Vertex vertex : graph2.getVertexList()) {
+            assertEquals(sort2[i], vertex.getName());
+            i++;
+        }
+    }
+
+    @Test
+    void testParseEdge() {
+        Graph graph2 = new AdjacencyMatrix();
+        String[] data = {
+            "E F edge5",
+            "B F edge6",
+            "B C edge2",
+            "A B edge1",
+            "C E edge7",
+            "C D edge3",
+            "A E edge4"
+        };
+        String[] cur2 = {"E", "F", "B", "C", "A", "D"};
+        for (String line : data) {
+            ParseDataFile.parseData(line,graph2,false);
+        }
+        int i = 0;
+        for (Vertex vertex : graph2.getVertexList()) {
+            assertEquals(cur2[i], vertex.getName());
+            i++;
+        }
+        i = 0;
+        graph2.topologicalSort();
+        String[] sort2 = {"A", "B", "C", "D", "E", "F"};
+        for (Vertex vertex : graph2.getVertexList()) {
+            assertEquals(sort2[i], vertex.getName());
+            i++;
+        }
+    }
 }
