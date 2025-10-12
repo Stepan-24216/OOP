3d2c7e8590d06ecb1aad7ab971abe633dc497dd3
Stepan-24216 <Stepan-24216@users.noreply.github.com>
2025-10-12 13:26:11 +0000

    Deploying to gh-pages from @ Stepan-24216/OOP@975266a8a5937beb41c0bddad8bad0ea450b2b5e 🚀

--- Task_2_1/package-search-index.js
+++ Task_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example.Functional"},{"l":"org.example.GraphStorageMethods"},{"l":"org.example.Objects"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example.functional"},{"l":"org.example.graphStorageMethods"},{"l":"org.example.objects"}];updateSearchResults();
