e5248539ce4666ae1ebd046dcd10c828ae2949ab
StepanEfimov <s.efimov@g.nsu.ru>
2025-11-13 13:23:12 +0700

    code style

--- Task_3_1/src/test/java/org/example/TestAlgorithm.java
+++ Task_3_1/src/test/java/org/example/TestAlgorithm.java
@@ -20,7 +20,7 @@ public class TestAlgorithm {
         AlgorithmBoyerMura a = new AlgorithmBoyerMura();
         int[] expected = {0, 2, 4, 6, 8, 10, 12};
         int i = 0;
-        for (int num : a.find("aba",filePath)) {
+        for (int num : a.find("aba", filePath)) {
             assertEquals(expected[i++], num);
         }
     }
@@ -29,10 +29,10 @@ public class TestAlgorithm {
      * Пустой файл.
      */
     @Test
-    void testEmptyFile() throws IOException{
+    void testEmptyFile() throws IOException {
         String filePath = "src/test/java/org/example/test1";
         AlgorithmBoyerMura a = new AlgorithmBoyerMura();
-        Set<Integer> test = a.find("aba",filePath);
+        Set<Integer> test = a.find("aba", filePath);
         assertTrue(test.isEmpty());
     }
 
@@ -40,10 +40,10 @@ public class TestAlgorithm {
      * Отсутствие шаблонов в тексте.
      */
     @Test
-    void testNothingPattern() throws IOException{
+    void testNothingPattern() throws IOException {
         String filePath = "src/test/java/org/example/test2";
         AlgorithmBoyerMura a = new AlgorithmBoyerMura();
-        Set<Integer> test = a.find("aba",filePath);
+        Set<Integer> test = a.find("aba", filePath);
         assertTrue(test.isEmpty());
     }
 
@@ -51,12 +51,14 @@ public class TestAlgorithm {
      * Обычный текст.
      */
     @Test
-    void testCommonFile() throws IOException{
+    void testCommonFile() throws IOException {
         String filePath = "src/test/java/org/example/test3";
         AlgorithmBoyerMura a = new AlgorithmBoyerMura();
-        int[] expected = {3, 8, 12, 18, 23, 27, 33, 38, 42, 48, 53, 57, 63, 68, 72, 78, 83, 87, 93, 98, 102, 108, 113, 117};
+        int[] expected =
+            {3, 8, 12, 18, 23, 27, 33, 38, 42, 48, 53, 57, 63, 68, 72, 78, 83, 87, 93, 98, 102, 108,
+                113, 117};
         int i = 0;
-        for (int num : a.find("cat",filePath)) {
+        for (int num : a.find("cat", filePath)) {
             assertEquals(expected[i++], num);
         }
     }
@@ -65,12 +67,12 @@ public class TestAlgorithm {
      * Обычный текст.
      */
     @Test
-    void testIntersectingChunk() throws IOException{
+    void testIntersectingChunk() throws IOException {
         String filePath = "src/test/java/org/example/test4";
         AlgorithmBoyerMura a = new AlgorithmBoyerMura();
         int[] expected = {10};
         int i = 0;
-        for (int num : a.find("aaaa",filePath)) {
+        for (int num : a.find("aaaa", filePath)) {
             assertEquals(expected[i++], num);
         }
     }
