17f0bd3fdc1b6a1c972d050e885410aa6d2406bc
Stepan-24216 <Stepan-24216@users.noreply.github.com>
2025-11-15 13:48:41 +0000

    Deploying to gh-pages from @ Stepan-24216/OOP@3814290a4a3471f04a3c0d73cc1610c94b997792 🚀

---
+++ Task_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Discipline"},{"p":"org.example","l":"Semester"},{"p":"org.example","l":"Student"},{"p":"org.example","l":"Trio"}];updateSearchResults();
